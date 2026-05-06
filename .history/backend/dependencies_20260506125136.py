2e2cf980b344891f8472040afc7a3794c5911130
Akash Pandey <pandeyakash85296@gmail.com>
2026-05-06 15:18:58 +0530

    auth commit

--- .history/backend/dependencies_20260506125136.py
+++ .history/backend/dependencies_20260506125136.py
@@ -2,7 +2,8 @@ from collections.abc import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database import SessionLocal
+from .database import SessionLocal
+
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with SessionLocal() as session:
--- .history/backend/schemas/auth_20260506125200.py
+++ .history/backend/schemas/auth_20260506125200.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, EmailStr
 
-from models.user import UserRole
+from ..models.user import UserRole
+
 
 class SignupRequest(BaseModel):
     username: str
